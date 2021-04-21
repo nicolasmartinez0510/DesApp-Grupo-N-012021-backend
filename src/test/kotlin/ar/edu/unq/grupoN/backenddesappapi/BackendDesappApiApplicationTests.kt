c9a1a53057f326a21357b3108bdd53cc98d07087
Nicolas Martinez <martinez.nicolas0510@gmail.com>
2021-04-21 16:26:06 -0300

    Agrego configuracion inicial de hibernate + runners

--- src/test/kotlin/ar/edu/unq/grupoN/backenddesappapi/BackendDesappApiApplicationTests.kt
+++ src/test/kotlin/ar/edu/unq/grupoN/backenddesappapi/BackendDesappApiApplicationTests.kt
@@ -3,11 +3,11 @@ package ar.edu.unq.grupoN.backenddesappapi
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-//@SpringBootTest
-//class BackendDesappApiApplicationTests {
-//
-//	@Test
-//	fun contextLoads() {
-//	}
-//
-//}
+@SpringBootTest
+class BackendDesappApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
