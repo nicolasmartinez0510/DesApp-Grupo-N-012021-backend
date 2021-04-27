a2a089de69f1c0c3447b21c95e6f7ef826d01929
Fede Sandoval <federico.sandoval@alu.unq.edu.ar>
2021-04-27 02:21:02 -0300

    create reviewDTO and refactors on Review class

--- src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/model/review/Review.kt
+++ src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/model/review/Review.kt
@@ -13,13 +13,14 @@ abstract class Review(contentInfo: ContentInfo, reviewInfo: ReviewInfo) {
     @ManyToOne(fetch = FetchType.EAGER)
     var cinematographicContent: CinematographicContent
     var platform: String
-    var isAChapterReview: IsAChapterReview
+    var reviewType: ReviewType
     var seasonNumber: Int? = null
     var episodeNumber: Int? = null
     var resumeText: String
     var text: String
     var rating: Rating
     var date: LocalDateTime
+    var language : String
     @OneToMany(cascade = [CascadeType.ALL], fetch = FetchType.EAGER)
     open var valorations: MutableSet<ValorationData> = mutableSetOf()
     @Id
@@ -36,7 +37,8 @@ abstract class Review(contentInfo: ContentInfo, reviewInfo: ReviewInfo) {
         this.text = reviewInfo.text
         this.rating = reviewInfo.rating
         this.date = reviewInfo.date
-        this.isAChapterReview = reviewInfo.isAChapterReview
+        this.language = reviewInfo.language
+        this.reviewType = reviewInfo.reviewType
     }
 
     fun rate(userId:String, platform:String, valoration:Valoration){
@@ -58,6 +60,8 @@ abstract class Review(contentInfo: ContentInfo, reviewInfo: ReviewInfo) {
     fun valoration():Int{
         return amountOf(Valoration.LIKE) - amountOf(Valoration.DISLIKE)
     }
+
+    open fun isPublic() = false
 
     private fun createValoration(userId: String, platform: String, valoration: Valoration) =
         ValorationData(this, userId, platform, valoration)
--- src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/service/dto/ReviewDTO.kt
+++ src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/service/dto/ReviewDTO.kt
@@ -1,4 +1,84 @@
 package ar.edu.unq.grupoN.backenddesappapi.service.dto
 
-class ReviewDTO {
+import ar.edu.unq.grupoN.backenddesappapi.model.ValorationData
+import ar.edu.unq.grupoN.backenddesappapi.model.review.*
+import java.time.LocalDateTime
+
+abstract class ReviewDTO(review: Review) {
+    var cinematographicContentTitleId: String
+    var platform: String
+    var language: String
+    var resumeText: String
+    var text: String
+    var rating: Rating
+    var date: LocalDateTime
+    var seasonNumber: Int? = null
+    var episodeNumber: Int? = null
+    var valorations: List<ValorationDTO>
+
+    companion object {
+        fun fromModel(review: Review): ReviewDTO {
+            return if (review.isPublic()) {
+                PublicDTO(review as Public)
+            } else {
+                PremiumDTO(review as Premium)
+            }
+        }
+    }
+
+    init {
+        this.cinematographicContentTitleId = review.cinematographicContent.titleId
+        this.platform = review.platform
+        this.language = review.language
+        this.resumeText = review.resumeText
+        this.text = review.text
+        this.rating = review.rating
+        this.date = review.date
+        this.valorations = review.valorations.map { ValorationDTO(it) }
+
+        checkIfChapterReview(review)
+    }
+
+    fun checkIfChapterReview(review: Review) {
+        if (review.reviewType == ReviewType.CHAPTER) {
+            this.seasonNumber = review.seasonNumber
+            this.episodeNumber = review.episodeNumber
+        }
+    }
+}
+
+class PublicDTO(public: Public) : ReviewDTO(public) {
+    var includeSpoiler: Boolean = false
+    var userId: String
+    var username: String
+    var geographicLocation: String
+
+    init {
+        this.includeSpoiler = public.includeSpoiler
+        this.userId = public.userId
+        this.username = public.username
+        this.geographicLocation = public.geographicLocation
+    }
+}
+
+class PremiumDTO(premium: Premium) : ReviewDTO(premium) {
+    var reviewerId: String
+
+    init {
+        this.reviewerId = premium.reviewerId
+    }
+}
+
+
+class ValorationDTO(valorationData: ValorationData) {
+    var userId: String
+    var platform: String
+    var valoration: Valoration
+
+    init {
+        this.userId = valorationData.userId
+        this.platform = valorationData.platform
+        this.valoration = valorationData.valoration
+    }
+
 }
