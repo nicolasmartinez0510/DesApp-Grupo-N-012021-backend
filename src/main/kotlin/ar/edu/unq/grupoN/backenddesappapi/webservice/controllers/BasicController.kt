498334a06c6e036b3d219e6b8cde3e7fc15c2803
Fede Sandoval <federico.sandoval@alu.unq.edu.ar>
2021-04-27 02:19:16 -0300

    refactor base controller

--- src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/webservice/controllers/BasicController.kt
+++ src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/webservice/controllers/BasicController.kt
@@ -1,22 +1,22 @@
 package ar.edu.unq.grupoN.backenddesappapi.webservice.controllers
 
-import ar.edu.unq.grupoN.backenddesappapi.model.review.Review
 import ar.edu.unq.grupoN.backenddesappapi.persistence.CinematographicContentRepository
 import ar.edu.unq.grupoN.backenddesappapi.persistence.ReviewRepository
 import ar.edu.unq.grupoN.backenddesappapi.service.dto.CinematographicContentDTO
+import ar.edu.unq.grupoN.backenddesappapi.service.dto.ReviewDTO
 import org.springframework.web.bind.annotation.GetMapping
 
 @ServiceREST
-class CinematographicController(val repository: CinematographicContentRepository, val reviewRepository: ReviewRepository) {
+class BasicController(val repository: CinematographicContentRepository, val reviewRepository: ReviewRepository) {
 
     @GetMapping("/content")
     fun content(): List<CinematographicContentDTO> {
         return repository.findAll().map { content -> CinematographicContentDTO.fromModel(content) }
     }
 
-    @GetMapping("/reviews")
-    fun reviews(): List<Review> {
-        return reviewRepository.findAll().toList()
+    @GetMapping("/review")
+    fun reviews(): List<ReviewDTO> {
+        return reviewRepository.findAll().map{ review -> ReviewDTO.fromModel(review) }
     }
 
 }
