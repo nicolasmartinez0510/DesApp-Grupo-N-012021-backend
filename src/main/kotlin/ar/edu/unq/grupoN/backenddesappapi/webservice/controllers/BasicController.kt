27c4d812822232a9f8ecabdaf7fe0c9c46431e67
Fede Sandoval <federico.sandoval@alu.unq.edu.ar>
2021-04-27 23:44:41 -0300

    modify basic endpoints

--- src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/webservice/controllers/BasicController.kt
+++ src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/webservice/controllers/BasicController.kt
@@ -1,22 +1,29 @@
 package ar.edu.unq.grupoN.backenddesappapi.webservice.controllers
 
 import ar.edu.unq.grupoN.backenddesappapi.persistence.CinematographicContentRepository
-import ar.edu.unq.grupoN.backenddesappapi.persistence.ReviewRepository
 import ar.edu.unq.grupoN.backenddesappapi.service.dto.CinematographicContentDTO
-import ar.edu.unq.grupoN.backenddesappapi.service.dto.ReviewDTO
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.view.RedirectView
+
+@RestController
+@CrossOrigin(origins = ["*"])
+class BasicController {
+
+    @GetMapping("/")
+    fun reviews(): RedirectView {
+        return RedirectView("/api/content")
+    }
+
+}
+
 
 @ServiceREST
-class BasicController(val repository: CinematographicContentRepository, val reviewRepository: ReviewRepository) {
+class Entry(val repository: CinematographicContentRepository) {
 
     @GetMapping("/content")
     fun content(): List<CinematographicContentDTO> {
         return repository.findAll().map { content -> CinematographicContentDTO.fromModel(content) }
     }
-
-    @GetMapping("/review")
-    fun reviews(): List<ReviewDTO> {
-        return reviewRepository.findAll().map{ review -> ReviewDTO.fromModel(review) }
-    }
-
 }
--- src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/webservice/controllers/ServiceREST.kt
+++ src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/webservice/controllers/ServiceREST.kt
@@ -1,9 +1,11 @@
 package ar.edu.unq.grupoN.backenddesappapi.webservice.controllers
 
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
 @CrossOrigin(origins = ["*"])
+@RequestMapping("/api")
 annotation class ServiceREST
