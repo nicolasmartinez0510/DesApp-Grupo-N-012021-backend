1b6efc4b9b53b472aee78ea7a30103d9ab47c795
Fede Sandoval <federico.sandoval@alu.unq.edu.ar>
2021-05-03 17:34:43 -0300

    ✨ change default path

--- src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/webservice/controllers/BasicController.kt
+++ src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/webservice/controllers/BasicController.kt
@@ -2,10 +2,9 @@ package ar.edu.unq.grupoN.backenddesappapi.webservice.controllers
 
 import ar.edu.unq.grupoN.backenddesappapi.persistence.CinematographicContentRepository
 import ar.edu.unq.grupoN.backenddesappapi.service.dto.CinematographicContentDTO
+import io.swagger.annotations.ApiOperation
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.view.RedirectView
 
 @RestController
@@ -13,12 +12,14 @@ import org.springframework.web.servlet.view.RedirectView
 @EnableAutoConfiguration
 class BasicController(val repository: CinematographicContentRepository) {
 
-    @GetMapping("/")
+    @ApiOperation(value = "Endpoint used to show Re-Senia API docs.", hidden = true)
+    @RequestMapping(value = ["/",""], method = [RequestMethod.GET])
     fun reviews(): RedirectView {
-        return RedirectView("/api/review")
+        return RedirectView("/swagger-ui.html")
     }
 
-    @GetMapping("/api/content")
+    @ApiOperation(value = "Endpoint used for api develop. to show generated fake reviews", hidden = true)
+    @RequestMapping(value = ["/api/content"], method = [RequestMethod.GET])
     fun content(): List<CinematographicContentDTO> {
         return repository.findAll().map { content -> CinematographicContentDTO.fromModel(content) }
     }
