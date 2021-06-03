df3510bb8636fc16557a2c7fb5eff40a3675d554
nicolasmartinez0510 <martinez.nicolas0510@gmail.com>
2021-06-02 23:29:20 -0300

    Agrego configuracion de CORS + descripcion de endpoint de usuario en swagger
    Co-Authored-By: Fede Sandoval <federico.sandoval@alu.unq.edu.ar>

--- src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/webservice/config/security/SecurityConfig.kt
+++ src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/webservice/config/security/SecurityConfig.kt
@@ -2,11 +2,17 @@ package ar.edu.unq.grupoN.backenddesappapi.webservice.config.security
 
 import ar.edu.unq.grupoN.backenddesappapi.service.PlatformAdminService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import javax.servlet.http.HttpServletRequest
 
 
 @Configuration
@@ -23,10 +29,31 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
             .antMatchers("/api/user/**").permitAll()
             .anyRequest().permitAll()
             .and()
+            .cors().configurationSource(corsConfigurationSource())
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
             .httpBasic()
             .and()
             .csrf().disable()
             .addFilterAfter(JWTAuthFilter(adminPlatformAdminService), UsernamePasswordAuthenticationFilter::class.java)
-        http.headers().frameOptions().disable()
+    }
+
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource? {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:3000",
+                                              "https://pacific-shelf-04003.herokuapp.com",
+                                              "https://agile-fortress-77406.herokuapp.com")
+        configuration.allowedMethods = listOf("GET", "POST")
+        configuration.allowCredentials = true
+        configuration.exposedHeaders = listOf("*")
+        configuration.allowedHeaders = listOf("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
+
+
--- src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/webservice/controllers/PlatformAdminController.kt
+++ src/main/kotlin/ar/edu/unq/grupoN/backenddesappapi/webservice/controllers/PlatformAdminController.kt
@@ -4,6 +4,7 @@ import ar.edu.unq.grupoN.backenddesappapi.service.PlatformAdminService
 import ar.edu.unq.grupoN.backenddesappapi.service.dto.LoginCredentialsRequest
 import ar.edu.unq.grupoN.backenddesappapi.service.dto.RegisterRequest
 import io.jsonwebtoken.Jwts
+import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.http.HttpHeaders
@@ -20,7 +21,9 @@ import org.springframework.web.bind.annotation.RequestMethod
 class PlatformAdminController {
     @Autowired
     private lateinit var platformAdminService: PlatformAdminService
-
+    @ApiOperation(
+        value = "For register in system and get a api key.This operation is available for all users."
+    )
     @RequestMapping(value = ["/register"], method = [RequestMethod.POST])
     fun register(@RequestBody registerRequest: RegisterRequest): ResponseEntity<*>? {
         val responseHeaders = HttpHeaders()
@@ -30,7 +33,9 @@ class PlatformAdminController {
             .headers(responseHeaders)
             .body("REGISTERED SUCCESSFULLY")
     }
-
+    @ApiOperation(
+        value = "For login in system, view and get a you api key.This operation is available for all users."
+    )
     @RequestMapping(value = ["/login"], method = [RequestMethod.POST])
     fun login(@RequestBody loginCredentialsRequest: LoginCredentialsRequest): ResponseEntity<*>? {
         val accessControlHeaders = platformAdminService.login(loginCredentialsRequest)
@@ -43,7 +48,9 @@ class PlatformAdminController {
             .headers(responseHeaders)
             .body("LOGGED IN SUCCESSFULLY")
     }
-
+    @ApiOperation(
+        value = "For see info of you user in system.This operation is available only for authenticated users"
+    )
     @RequestMapping(value=["/me"], method = [RequestMethod.GET])
     fun userInfo(@RequestHeader("Authentication") authHeader: String): ResponseEntity<*>? {
         return ResponseEntity.ok(platformAdminService.findByToken(authHeader))
